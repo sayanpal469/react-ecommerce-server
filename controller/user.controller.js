593a81545f4eae2511bb17cd6e9c639b4c1c3215
sayanpal469 <sayanpal469@gmail.com>
2024-04-07 06:02:40 +0530

    update and fetfh user apis updated

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,8 +3,11 @@ const { User } = require("../model/user.model");
 const fetchUserById = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = await User.findById(id, "name email addresses orders").exec();
-    res.status(201).json(user);
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ status: false, message: "User not found" });
+    }
+    res.status(200).json({ status: true, user });
   } catch (error) {
     // console.error(error);
     res.status(400).json(error.message);
@@ -13,14 +16,12 @@ const fetchUserById = async (req, res) => {
 
 const updateUser = async (req, res) => {
   try {
+    console.log(req.body);
     const email = req.body.email;
-    const user = await User.findOneAndUpdate(
-      { email: email },
-      { password: req.body.password },
-      {
-        new: true,
-      }
-    );
+    const data = req.body;
+    const user = await User.findOneAndUpdate({ email: email }, data, {
+      new: true,
+    });
 
     if (!user) {
       return res.status(404).json({ status: false, message: "User not found" });
